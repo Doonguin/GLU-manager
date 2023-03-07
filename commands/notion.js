091cf4632503eeb77d798b36a41a7dc271a00076
frog.com <210587@student.glu.nl>
2023-03-07 20:15:57 +0100

    update

--- commands/notion.js
+++ commands/notion.js
@@ -2,13 +2,20 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 // Build slash command
-const notion =
+const bureau =
+  "https://www.notion.so/het-bureau/Het-BUREAU-1b3b8358bc184ac29e93dcdaf5823369";
+const webdev =
   "https://gluwebdev.notion.site/Module-17-NodeJs-13b46f243799434daec96ad8acedf23a";
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("notion")
-    .setDescription("de notion link van de module waar we mee bezig zijn"),
-
+    .setDescription("de notion link van de module waar we mee bezig zijn")
+    .addSubcommand((subcommand) =>
+      subcommand.setName("bureau").setDescription("the notion of het bureau")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand.setName("webdev").setDescription("de notion of webdev")
+    ),
   async execute(interaction) {
     await interaction.reply(notion);
   },
