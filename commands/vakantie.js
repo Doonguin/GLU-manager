a10f7d6441a547ec0ae425ed7f15e2eb87ca8d51
ProsperityGH <90307071+ProsperityGH@users.noreply.github.com>
2023-03-07 13:38:59 +0100

    vakantie command (hardcoded)

--- commands/vakantie.js
+++ commands/vakantie.js
@@ -2,12 +2,14 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 // Build slash command
+const notion =
+  "Meivakantie: 29 april 2023 t/m 7 mei 2023";
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("test")
-    .setDescription("dit is een test"),
+    .setName("vakantie")
+    .setDescription("de volgende vakantie"),
 
   async execute(interaction) {
-    await interaction.reply("tost");
+    await interaction.reply(notion);
   },
 };
