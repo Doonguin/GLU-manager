3ac0b6304da27ff32b7d3cb7c1c70e0cf7dc500a
ProsperityGH <90307071+ProsperityGH@users.noreply.github.com>
2023-03-14 10:57:22 +0100

    Update notion.js

--- commands/notion.js
+++ commands/notion.js
@@ -2,21 +2,32 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 // Build slash command
-const bureau =
-  "https://www.notion.so/het-bureau/Het-BUREAU-1b3b8358bc184ac29e93dcdaf5823369";
-const webdev =
-  "https://gluwebdev.notion.site/Module-17-NodeJs-13b46f243799434daec96ad8acedf23a";
+const bureauURL = "https://www.notion.so/het-bureau/Het-BUREAU-1b3b8358bc184ac29e93dcdaf5823369";
+const webdevURL = "https://gluwebdev.notion.site/gluwebdev/Leerjaar-2022-2023-a285e26687dc48a88446842ca5da6a06";
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("notion")
-    .setDescription("de notion link van de module waar we mee bezig zijn")
+    .setDescription("De Notion link van de module waar we mee bezig zijn")
     .addSubcommand((subcommand) =>
-      subcommand.setName("bureau").setDescription("the notion of het bureau")
+      subcommand.setName("bureau").setDescription("De Notion van Het Bureau")
     )
     .addSubcommand((subcommand) =>
-      subcommand.setName("webdev").setDescription("de notion of webdev")
+      subcommand.setName("webdev").setDescription("De Notion van Webdev")
     ),
   async execute(interaction) {
+    const subcommand = interaction.options.getSubcommand();
+    let notion = "";
+    switch (subcommand) {
+      case "bureau":
+        notion = bureauURL;
+        break;
+      case "webdev":
+        notion = webdevURL;
+        break;
+      default:
+        notion = "Ongeldige subcommando";
+    }
     await interaction.reply(notion);
   },
 };
